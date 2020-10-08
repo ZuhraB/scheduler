f26d919278f41a211cfb317da52984a69b543ae3
ZB <zb@ZBs-MacBook-Air.local>
2020-10-07 22:53:18 -0400

    added the READMEfile and docs

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -20,11 +20,10 @@ export default function DayListItem(props) {
   };
 
   return (
-    <li data-testid="day"
+    <li className={dayClass} 
     onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
       <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
 }
-//className={dayClass} 
